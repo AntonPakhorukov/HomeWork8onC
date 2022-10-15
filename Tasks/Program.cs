c6428767737fe3110c2a2e6eee1447caeaeffbaa
ÄntonPakhorukov <pakhorukov.a@mail.ru>
2022-10-16 01:19:34 +0700

    all tasks is finish

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -28,7 +28,8 @@
 18 20
 15 18
 
-Дополнительная задача (60): Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Дополнительная задача (60): Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 Массив размером 2 x 2 x 2
 66(0,0,0) 25(0,1,0)
 34(1,0,0) 41(1,1,0)
@@ -95,6 +96,12 @@ switch (task)
         break;
     case 58:
         Task58();
+        break;
+    case 60:
+        Task60();
+        break;
+    case 62:
+        Task62();
         break;
 }
 void Task54()
@@ -212,7 +219,7 @@ void Task58()
     {
         size = matrix2.GetLength(1);
     }
-    
+
     void PrintTwoMatrix(int[,] inputMatrix1, int[,] inputMatrix2, int size)
     {
         for (int i = 0; i < inputMatrix1.GetLength(0); i++)
@@ -221,14 +228,16 @@ void Task58()
             {
                 Console.Write(inputMatrix1[i, j] + " ");
             }
-            
+
             Console.Write(" |  ");
             for (int k = 0; k < inputMatrix2.GetLength(1); k++)
             {
                 if (i < inputMatrix2.GetLength(0)) // додбавил if чтобы вывести второй массив с меншими строками
                 {
                     Console.Write(inputMatrix2[i, k] + " ");
-                } else {
+                }
+                else
+                {
                     continue;
                 }
             }
@@ -263,4 +272,120 @@ void Task58()
     int[,] result = multiplyMatrix(matrix1, matrix2, size);
     Console.WriteLine();
     PrintBinaryArray(result);
+}
+void Task60()
+{
+    /*
+    Дополнительная задача (60): Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+    Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+    Массив размером 2 x 2 x 2
+    66(0,0,0) 25(0,1,0)
+    34(1,0,0) 41(1,1,0)
+    27(0,0,1) 90(0,1,1)
+    26(1,0,1) 55(1,1,1)
+    */
+    Console.Clear();
+    Console.Write("Введите первое значение трехмернорго массива: ");
+    int first = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите второе значение трехмерного массива: ");
+    int second = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите третье значение трехмерного массива: ");
+    int thied = Convert.ToInt32(Console.ReadLine());
+    int[,,] array3 = new int[first, second, thied];
+    for (int i = 0; i < array3.GetLength(0); i++)
+    {
+        for (int j = 0; j < array3.GetLength(1); j++)
+        {
+            for (int k = 0; k < array3.GetLength(2); k++)
+            {
+                array3[j, k, i] = new Random().Next(1, 100);
+                Console.Write(array3[j, k, i] + "(" + j + "," + k + "," + i + ") ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+void Task62()
+{
+    /*
+    Дополнительная задача 2 (62):. Напишите программу, 
+    которая заполнит спирально массив 4 на 4.
+    Например, на выходе получается вот такой массив:
+    01 02 03 04
+    12 13 14 05
+    11 16 15 06
+    10 09 08 07
+    */
+    Console.Clear();
+    void PrintBinaryArray2(int[,] inputArray)
+    {
+        for (int i = 0; i < inputArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < inputArray.GetLength(1); j++)
+            {
+                if (inputArray[i, j] < 10)
+                {
+                    Console.Write($"0{inputArray[i, j]} ");
+                }
+                else
+                {
+                    Console.Write(inputArray[i, j] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+    // Console.Write("Введите размер двумерного массива: ");
+    // int size = Convert.ToInt32(Console.ReadLine());
+    int[,] array = new int[4, 4];
+    int[,] FillSpiralArray(int[,] inputArray)
+    {
+        int number1 = 1;
+        int number2 = inputArray.GetLength(1);
+        int number3 = number2 * 2 - number1;
+        int number4 = number3 + number2 - number1;
+        for (int i = 0; i < inputArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < inputArray.GetLength(1); j++)
+            {
+                if (i == 0)
+                {
+                    inputArray[i, j] = number1;
+                    number1++;
+                }
+                else if (i == inputArray.GetLength(0) - 1)
+                {
+                    inputArray[j, i] = number1 - 1;
+                    inputArray[i, j] = number3 + number3 - number1 + 1;
+                    number1++;
+                }
+                else if (i > 0 && i < inputArray.GetLength(1) - 1)
+                {
+                    inputArray[i, 0] = number4 + number2 - i - 1;
+                }
+            }
+        }
+        for (int i = 1; i < inputArray.GetLength(0) - 1; i++)
+        {
+            for (int j = 1; j < inputArray.GetLength(1) - 1; j++)
+            {
+                if (i == 1)
+                {
+                    inputArray[i, j] = inputArray[1, 0] + j;
+                }
+                else if (i == inputArray.GetLength(0) - inputArray[0,1] && j > 1 && j < inputArray.GetLength(1)-1)
+                {
+                    inputArray[j, i] = inputArray[1,0] + inputArray[0,inputArray.GetLength(0) - inputArray[0,3] + j];
+                 } else if (i == inputArray.GetLength(0) - inputArray[0,1]) {
+                    for (int k = 2; k > 0; k--) {
+                        inputArray[i,k] = inputArray.GetLength(0) * inputArray.GetLength(1);
+                    }                    
+                }
+            }
+        }
+        return inputArray;
+    }
+    array = FillSpiralArray(array);
+    Console.WriteLine();
+    PrintBinaryArray2(array);
 }
